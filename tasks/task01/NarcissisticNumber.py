fd4cc75553a35581d901541d6dc314f401742fa5
AlexanderBestsenny <abestsenny@exadel.com>
2021-09-13 21:23:11 +0300

    Task01- ver3.

--- tasks/task01/NarcissisticNumber.py
+++ tasks/task01/NarcissisticNumber.py
@@ -1,15 +1,11 @@
 for k in range(1, 1001):
-    n = 0
     numberToProcess = k
     listOfNumbers = list()
-
     while numberToProcess != 0:
         listOfNumbers.append(numberToProcess % 10)
         numberToProcess = numberToProcess // 10
-        n += 1
-
     sumOfDigits = 0
     for i in listOfNumbers:
-        sumOfDigits += i ** n
+        sumOfDigits += i ** len(listOfNumbers)
     if sumOfDigits == k:
-        print("{}, n={}".format(k, n))
+        print("{}, n={}".format(k, len(listOfNumbers)))
